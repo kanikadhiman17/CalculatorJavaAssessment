b258e10eeb2e9c3305475a8747e7d426e322cd07
Kanika Dhiman <dhimankanika17@gmail.com>
2020-09-23 17:29:08 +0530

    Use Collectors.summingInt

--- src/main/java/com/kanika/calculatorJavaAssessment/operators/DefaultAdder.java
+++ src/main/java/com/kanika/calculatorJavaAssessment/operators/DefaultAdder.java
@@ -2,30 +2,33 @@ package com.kanika.calculatorJavaAssessment.operators;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class DefaultAdder implements Adder{
     @Override
     public Integer add(List<Integer> numbers) {
-    	// Adding the numbers in the list
-    	
-    	if(numbers.isEmpty())
-    		throw new IllegalStateException();
+        // Throw exception if the list is empty
+        if(numbers.isEmpty())
+            throw new IllegalStateException();
 
-    	// Generalized Method
-		/*Integer sum=0;
-		for(int i=0; i<numbers.size(); i++)
-    		sum+=numbers.get(i);
+        // Generalized Method
+        /*Integer sum=0;
+        for(int i=0; i<numbers.size(); i++)
+            sum+=numbers.get(i);
 
-    	return sum;*/
+        return sum;*/
 
-		// Using Lambdas
-    	/*AtomicReference<Integer> sum = new AtomicReference<>(0);
-    	numbers.forEach(i -> sum.updateAndGet(v -> v + i));
+        // Using Lambdas
+        /*AtomicReference<Integer> sum = new AtomicReference<>(0);
+        numbers.forEach(i -> sum.updateAndGet(v -> v + i));
 
-    	return sum.get();*/
+        return sum.get();*/
 
-    	// Using Stream
-		return numbers.stream().reduce(0, (a,b)->a+b);
+        // Using Stream
+        //return numbers.stream().reduce(0, (sum,i)->sum+i);
+
+        // Using Collectors.summingInt
+        return numbers.stream().collect(Collectors.summingInt(p -> p));
 
     }
 }
--- src/main/java/com/kanika/calculatorJavaAssessment/operators/DefaultMultiplier.java
+++ src/main/java/com/kanika/calculatorJavaAssessment/operators/DefaultMultiplier.java
@@ -2,27 +2,30 @@ package com.kanika.calculatorJavaAssessment.operators;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class DefaultMultiplier implements Multiplier {
     @Override
     public Integer multiply(List<Integer> numbers) {
-    	// Multiplying the numbers in the list
-    	if(numbers.isEmpty())
-    		throw new IllegalStateException() ;
+		// Throw exception if the list is empty
+		if(numbers.isEmpty())
+			throw new IllegalStateException() ;
     	
-    	// Generalized Method
-    	/*Integer mul=1;
-    	for(int i=0; i<numbers.size(); i++)
-    		mul.updateAndGet(v -> v * numbers.get(i));
-    	return mul;*/
+		// Generalized Method
+		/*Integer mul=1;
+		for(int i=0; i<numbers.size(); i++)
+			mul.updateAndGet(v -> v * numbers.get(i));
+		return mul;*/
 
 		// Using Lambdas
 		/*AtomicReference<Integer> mul= new AtomicReference<>(1);
-    	numbers.forEach(i -> mul.updateAndGet(v -> v * i));
-    	return mul.get();*/
+		numbers.forEach(i -> mul.updateAndGet(v -> v * i));
+		return mul.get();*/
 
-    	// Using Stream
+		// Using Stream
 		return numbers.stream().reduce(1, (a,b)->a*b);
+
+
 
 	}
 }
